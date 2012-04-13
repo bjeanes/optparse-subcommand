a356a33e6ba9f00f1a3ca31ebadb1216a2adc5dc
Bodaniel Jeanes <me@bjeanes.com>
2012-04-12 22:54:03 -0500

    I accidentally an implementation

--- lib/optparse/subcommand.rb
+++ lib/optparse/subcommand.rb
@@ -2,6 +2,32 @@ require 'optparse'
 
 class OptionParser
   module Subcommand
+    def subcommand(key, &block)
+      subcommand = lambda { OptionParser.new(&block) }
+      subcommands[key.to_s] = subcommand
+    end
+
+    private
+
+    def self.included(klass)
+      klass.instance_eval do
+        alias_method :parse_without_subcommand!, :parse!
+        alias_method :parse!, :parse_with_subcommand!
+      end
+    end
+
+    def parse_with_subcommand!(argv = default_argv)
+      subcommand = catch(:subcommand) do
+        return order!(argv) { |sub| throw :subcommand, sub }
+      end
+
+      sub_parser = subcommands[subcommand.to_s]
+      sub_parser.call.send(:parse!, argv) if sub_parser
+    end
+
+    def subcommands
+      @subcommands ||= {}
+    end
   end
 
   include Subcommand
--- spec/optparse/subcommand_spec.rb
+++ spec/optparse/subcommand_spec.rb
@@ -1,4 +1,41 @@
 require 'optparse/subcommand'
 
 describe OptionParser do
+  def run(args)
+    options.parse(args.split(/\s+/))
+    hits
+  end
+
+  let(:hits) { [] }
+  let(:options) do
+    OptionParser.new do |opt|
+      opt.on '-t' do
+        hits << :t
+      end
+
+      opt.subcommand 'foo' do |foo|
+        hits << :foo
+
+        foo.on '-q' do
+          hits << :foo_q
+        end
+      end
+
+      opt.subcommand 'bar' do |bar|
+        hits << :bar
+
+        bar.on '-s' do
+          hits << :bar_s
+        end
+      end
+    end
+  end
+
+  specify { run("-t").should == [:t] }
+  specify { run("foo").should == [:foo] }
+  specify { run("foo -q").should == [:foo, :foo_q] }
+  specify { run("foo bar").should == [:foo] }
+  specify { run("-t foo").should == [:t, :foo] }
+
+  specify { lambda { run("foo -t") }.should raise_error(OptionParser::InvalidOption )}
 end
