59533b3b82a4f070857e43b5454819c9edf29d54
Jason Karns <jason@karns.name>
2017-04-11 10:36:37 -0400

    Rename 'run' as 'parse' to better describe its role

--- spec/optparse/subcommand_spec.rb
+++ spec/optparse/subcommand_spec.rb
@@ -1,7 +1,7 @@
 require 'optparse/subcommand'
 
 describe OptionParser do
-  def run(args)
+  def parse(args)
     options.parse(args)
     hits
   end
@@ -31,13 +31,13 @@ describe OptionParser do
     end
   end
 
-  specify { expect(run(%w[-t])).to eq([:t]) }
-  specify { expect(run(%w[foo])).to eq([:foo]) }
-  specify { expect(run(%w[foo -q])).to eq([:foo, :foo_q]) }
-  specify { expect(run(%w[foo bar])).to eq([:foo]) }
-  specify { expect(run(%w[-t foo])).to eq([:t, :foo]) }
+  specify { expect(parse(%w[-t])).to eq([:t]) }
+  specify { expect(parse(%w[foo])).to eq([:foo]) }
+  specify { expect(parse(%w[foo -q])).to eq([:foo, :foo_q]) }
+  specify { expect(parse(%w[foo bar])).to eq([:foo]) }
+  specify { expect(parse(%w[-t foo])).to eq([:t, :foo]) }
 
-  specify { expect { run(%w[foo -t]) }.to raise_error(OptionParser::InvalidOption )}
+  specify { expect { parse(%w[foo -t]) }.to raise_error(OptionParser::InvalidOption )}
 
   specify { expect(options.to_s).to match(/foo/) }
 end
