4f6dc2daf09f65459a0424e8b10fe969a4fd52e1
Jason Karns <jason@karns.name>
2017-04-11 10:33:02 -0400

    provide args as array, so no need to split

--- spec/optparse/subcommand_spec.rb
+++ spec/optparse/subcommand_spec.rb
@@ -2,7 +2,7 @@ require 'optparse/subcommand'
 
 describe OptionParser do
   def run(args)
-    options.parse(args.split(/\s+/))
+    options.parse(args)
     hits
   end
 
@@ -31,13 +31,13 @@ describe OptionParser do
     end
   end
 
-  specify { expect(run("-t")).to eq([:t]) }
-  specify { expect(run("foo")).to eq([:foo]) }
-  specify { expect(run("foo -q")).to eq([:foo, :foo_q]) }
-  specify { expect(run("foo bar")).to eq([:foo]) }
-  specify { expect(run("-t foo")).to eq([:t, :foo]) }
+  specify { expect(run(%w[-t])).to eq([:t]) }
+  specify { expect(run(%w[foo])).to eq([:foo]) }
+  specify { expect(run(%w[foo -q])).to eq([:foo, :foo_q]) }
+  specify { expect(run(%w[foo bar])).to eq([:foo]) }
+  specify { expect(run(%w[-t foo])).to eq([:t, :foo]) }
 
-  specify { expect { run("foo -t") }.to raise_error(OptionParser::InvalidOption )}
+  specify { expect { run(%w[foo -t]) }.to raise_error(OptionParser::InvalidOption )}
 
   specify { expect(options.to_s).to match(/foo/) }
 end
